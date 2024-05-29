26797d88f4060a4bb947417952d8176f854a25b8
Milton Black <arziblack2@gmail.com>
2024-05-29 07:50:21 -0700

    feat: updated custom color theming...

--- src/theme.ts
+++ src/theme.ts
@@ -3,9 +3,10 @@ import { extendTheme } from "@chakra-ui/react";
 export const theme = extendTheme({
   colors: {
     brand: {
-      light: "blue",
+      page: "#E5E5E5",
+      light: "#5D5A6F",
       dark: "#0A033C",
-      red: "#0A033C",
+      red: "#FF6652",
     },
   },
 });
