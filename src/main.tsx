a6ca0dd2d19c8372a26fbc3658a6106ff4807041
pherbcampton <tuinperisamuel@yahoo.com>
2024-05-29 11:53:00 +0100

    added yarn, and also configured Chakra

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-  colors: {
-    brand: {
-      light: "#9C4DF4",
-      dark: "#0A033C",
-      red: "#0A033C"
-    },
-  },
-})
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
