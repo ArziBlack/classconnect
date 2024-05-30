3274efb797ed20b83e1ce83e35297cc72f68efe4
Milton Black <arziblack2@gmail.com>
2024-05-30 03:51:47 -0700

    feat:signup setup...

--- src/app/store.ts
+++ src/app/store.ts
@@ -9,4 +9,4 @@ export const store = configureStore({
         auth: authReducer,
         other: otherReducer
     }
-})
+});
--- src/components/Signup/PageOne.tsx
+++ src/components/Signup/PageOne.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Box, FormLabel, Input, Button } from '@chakra-ui/react'
 
-const PageOne = () => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const PageOne = ({ data, onChange, onClick }: any) => {
   return (
-    <div>PageOne</div>
+    <>
+      <Box w='100%' mb={6}>
+        <FormLabel fontWeight='bold' fontSize='15px'>First name</FormLabel>
+        <Input fontSize='15' name="firstName" required type='name' value={data.firstName} onChange={onChange} />
+      </Box>
+      <Box w='100%' mb={6}>
+        <FormLabel fontWeight='bold' fontSize='15px'>Last name</FormLabel>
+        <Input fontSize='15' name="lastName" type='name' value={data.lastName} onChange={onChange} />
+      </Box>
+      <Box w='100%' mb={6}>
+        <FormLabel fontWeight='bold' fontSize='15px'>Email</FormLabel>
+        <Input fontSize='15' name="email" type='email' value={data.email} required onChange={onChange} />
+      </Box>
+
+      <Button onClick={()=> onClick("pagetwo")} borderRadius='md' color='white' bg='#002C8A' width='300px'>Next</Button>
+    </>
   )
 }
 
