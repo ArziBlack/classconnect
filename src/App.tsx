1c6bb0822e3f5a702a43f209c87279985d91c06c
MONEYBAG <110569376+King-Lord@users.noreply.github.com>
2024-05-31 16:50:33 +0200

    apply-instructor done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-
-
 import Footer from "./components/Footer";
 import NewsletterSection from "./components/NewsletterSection";
+import ApplyInstructor from "./components/ApplyInstructor";
 
 function App() {
   return (
     <div>
+      <ApplyInstructor />
       <NewsletterSection />
       <Footer />
     </div>
-    );
-    }
-  export default App;
-
-
+  );
+}
+export default App;
