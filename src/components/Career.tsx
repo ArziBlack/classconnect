87be67ff9f83f75b6d1d6a2d0b760068fd679c6e
Milton Black <arziblack2@gmail.com>
2024-06-01 04:30:50 -0700

    feat: made Career page responsive...

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -6,50 +6,50 @@ import { CAREER } from "../constants/illlustrations";
 const Career = () => {
   return (
     <Flex
-      w={`full`}
-      justifyContent={`space-between`}
+      w="full"
+      justifyContent="space-between"
       paddingTop={{ base: "20", md: "58" }}
       flexDir={{ base: "column-reverse", md: "row-reverse" }}
       alignItems="center"
-      // bg={`brand.pagedark`}
-      h="550px"
-      borderRadius={`20px`}
+      h={{ base: "auto", md: "550px" }} // Adjusted height for responsiveness
+      borderRadius="20px"
       marginY="40px"
     >
       <Box
-        flex="1"
+        flex={{ base: "none", md: 1 }} // Adjusted flex basis
         display="flex"
-        flexDir={`column`}
+        flexDir={{ base: "column", md: "row" }} // Adjusted flex direction
         alignItems={{ base: "center", md: "flex-start" }}
-        justifyContent={`space-between`}
+        justifyContent="space-between"
         paddingY={{ base: "10", md: "20" }}
         textAlign={{ base: "center", md: "left" }}
-        p={20}
+        p={{ base: 4, md: 20 }} // Adjusted padding
       >
-        <Heading
-          // fontSize={{ base: "2xl", md: "xxx-large" }}
-          size={{ base: "xl", md: "3xl" }}
-          as="h2"
-          fontWeight={600}
-          color={`brand.dark`}
-          paddingRight={{ base: "0", md: "20px" }}
-          paddingY={{ base: "5", md: "0" }}
-        >
-          Want to share your knowledge? Join us a Mentor
-        </Heading>
-        <Text fontSize={`medium`} color={`brand.offwhite`} paddingY={`10px`}>
-          High-definition video is video of higher resolution and quality than
-          standard-definition. While there is no standardized meaning for
-          high-definition, generally any video.
-        </Text>
-        <Button text="Career Information" />
+        <Box flex="1">
+          <Heading
+            size={{ base: "xl", md: "3xl" }}
+            as="h2"
+            fontWeight={600}
+            color="brand.dark"
+            paddingRight={{ base: "0", md: "20px" }}
+            paddingY={{ base: "5", md: "0" }}
+          >
+            Want to share your knowledge? Join us as a Mentor
+          </Heading>
+          <Text fontSize="medium" color="brand.offwhite" paddingY="10px">
+            High-definition video is video of higher resolution and quality than
+            standard-definition. While there is no standardized meaning for
+            high-definition, generally any video.
+          </Text>
+          <Button text="Career Information" />
+        </Box>
       </Box>
       <Box
-        flex="1"
+        flex={{ base: "none", md: 1 }} // Adjusted flex basis
         display="flex"
         justifyContent="flex-end"
         alignItems="flex-end"
-        marginY={`10px`}
+        marginY="10px"
         h="full"
         w="full"
         p="15px"
@@ -58,7 +58,7 @@ const Career = () => {
           src={CAREER}
           h={{ base: "300px", md: "500px" }}
           w={{ base: "300px", md: "500px" }}
-          objectFit={`contain`}
+          objectFit="contain"
         />
       </Box>
     </Flex>
