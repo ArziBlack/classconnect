29e871d898b941c33aa64b6c0b3e30188cc2617e
Milton Black <arziblack2@gmail.com>
2024-06-03 16:01:56 +0100

    fix: links in courses...

--- src/utils/course.ts
+++ src/utils/course.ts
@@ -2,36 +2,36 @@ export const courseLinks = [
     {
         id: 1,
         route:"All Courses",
-        link: "/"
+        link: "/courses"
     },
     {
         id: 2,
         route:"kindergarten",
-        link: "/kindergarten"
+        link: "kindergarten"
     },
     {
         id: 3,
         route:"Computer",
-        link: "/computer"
+        link: "computer"
     },
     {
         id: 4,
         route:"Science",
-        link: "/science"
+        link: "science"
     },
     {
         id: 5,
         route:"Engineering",
-        link: "/engineering"
+        link: "engineering"
     },
     {
         id: 6,
         route:"High School",
-        link: "/high-school"
+        link: "high-school"
     },
     {
         id: 7,
         route:"college",
-        link: "/college"
+        link: "college"
     },
 ]
