ab316a08b1dad74a757967e9064b5d9026e7f1a0
Milton Black <arziblack2@gmail.com>
2024-06-09 14:35:03 +0100

    feat: adding types for signup...

--- src/typings/signup.ts
+++ src/typings/signup.ts
@@ -1,4 +1,14 @@
 export interface IGuardian {
     first_name: string,
-    last_name: string
+    last_name: string,
+    student_email: string,
+    sex: string,
+    country: string,
+    state: string,
+    course: string,
+    dateOfBirth: Date,
+    classTime_options: Array<string>,
+    payment_plan: string,
+    class_type: string,
+    salutation: string,
 }
