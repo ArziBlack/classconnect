5f0092f5298c16739917a2e78714ee8e497aedf6
MONEYBAG <110569376+King-Lord@users.noreply.github.com>
2024-06-12 06:04:47 +0100

    Clinton/customcomponent-fixed

--- src/components/CustomComponent.tsx
+++ src/components/CustomComponent.tsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from "react";
 import PropTypes from "prop-types";
 import {
   Box,
@@ -7,104 +8,147 @@ import {
   IconButton,
   AspectRatio,
 } from "@chakra-ui/react";
-import {
-  //  FaPlay,
-  //  FaPause,
-  FaStepForward,
-  FaStepBackward,
-} from "react-icons/fa";
-import Pattern from "../assets/images/Pattern.png";
+import { FaStepForward, FaStepBackward, FaPlay, FaPause } from "react-icons/fa";
+import Pattern from "../assets/images/Pattern.png"; // Adjust the path according to your project structure
 
-const YoutubeEmbed = ({ embedId }) => (
-  <Box maxW="1024px" mx="auto" px={{ base: 4, md: 8 }} position="relative">
+const YoutubeEmbed = ({ initialEmbedId, videoList }) => {
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(
+    videoList.indexOf(initialEmbedId)
+  );
+  const [isPlaying, setIsPlaying] = useState(true);
+  const iframeRef = useRef(null);
+
+  const handlePlayPause = () => {
+    if (iframeRef.current) {
+      const iframe = iframeRef.current;
+      if (isPlaying) {
+        iframe.contentWindow.postMessage(
+          '{"event":"command","func":"pauseVideo","args":""}',
+          "*"
+        );
+      } else {
+        iframe.contentWindow.postMessage(
+          '{"event":"command","func":"playVideo","args":""}',
+          "*"
+        );
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentVideoIndex < videoList.length - 1) {
+      setCurrentVideoIndex(currentVideoIndex + 1);
+      setIsPlaying(true);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentVideoIndex > 0) {
+      setCurrentVideoIndex(currentVideoIndex - 1);
+      setIsPlaying(true);
+    }
+  };
+
+  return (
     <Box
-      zIndex={1}
-      position="absolute"
-      right={{ base: -4, md: -12 }}
-      top={{ base: -4, md: -12 }}
-      width={{ base: "100px", md: "150px" }}
-    >
-      <Img src={Pattern} />
-    </Box>
-    <Heading as="h2" size="lg" fontWeight="bold" textAlign="center" mb={4}>
-      How to apply to join as instructor
-    </Heading>
-    {/* <YouTube videoId={videoId} /> */}
-    <iframe
-      width="1024"
-      height="515"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      style={{ position: "relative" }}
-      title="Embedded youtube"
-    />
-    <Box
-      p={5}
-      bg="white"
-      borderRadius="22px"
-      boxShadow="lg"
+      maxW="1024px"
+      mx="auto"
+      px={{ base: 4, md: 8 }}
+      mt={{ base: 4, md: 8 }}
+      mb={{ base: 4, md: 8 }}
       position="relative"
-      zIndex={2}
     >
-      <Box borderRadius="22px" overflow="hidden">
-        <AspectRatio ratio={16 / 9}>
-          <div id="youtube-player" />
-        </AspectRatio>
-        <Flex
-          position="absolute"
-          bottom={0}
-          left={0}
-          right={0}
-          align="center"
-          borderBottomRightRadius="22px"
-          borderBottomLeftRadius="22px"
-          justifyContent="center"
-          bg="rgba(0, 0, 0, 0.5)"
-          p={2}
-        >
-          <IconButton
-            aria-label="Previous"
-            icon={<FaStepBackward />}
-            variant="ghost"
-            color="white"
-            // onClick={handlePrevious}
-            sx={{ "&:hover": { bg: "transparent" } }}
-          />
-          <IconButton
-            aria-label={`{isPlaying ? "Pause" : "Play"}`}
-            // icon={`{isPlaying ? <FaPause /> : <FaPlay />}`}
-            variant="ghost"
-            color="white"
-            // onClick={handlePlayPause}
-            sx={{ "&:hover": { bg: "transparent" } }}
-            mx={2}
-          />
-          <IconButton
-            aria-label="Next"
-            icon={<FaStepForward />}
-            variant="ghost"
-            color="white"
-            // onClick={handleNext}
-            sx={{ "&:hover": { bg: "transparent" } }}
-          />
-        </Flex>
+      <Box
+        zIndex={0}
+        position="absolute"
+        right={{ base: -4, md: -12 }}
+        top={{ base: "4", md: "4" }}
+        width={{ base: "100px", md: "150px" }}
+      >
+        <Img src={Pattern} alt="Pattern background" />
+      </Box>
+
+      <Heading as="h2" size="lg" fontWeight="bold" textAlign="center" mb={4}>
+        How to apply to join as instructor
+      </Heading>
+
+      <Box
+        bg="white"
+        borderRadius="22px"
+        boxShadow="lg"
+        position="relative"
+        zIndex={2}
+      >
+        <Box borderRadius="22px" overflow="hidden">
+          <AspectRatio ratio={16 / 9}>
+            <iframe
+              ref={iframeRef}
+              src={`https://www.youtube.com/embed/${videoList[currentVideoIndex]}?enablejsapi=1`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Embedded YouTube video"
+            />
+          </AspectRatio>
+          <Flex
+            position="absolute"
+            bottom={0}
+            left={0}
+            right={0}
+            align="center"
+            borderBottomRightRadius="22px"
+            borderBottomLeftRadius="22px"
+            justifyContent="center"
+            bg="rgba(0, 0, 0, 0.5)"
+            p={2}
+          >
+            <IconButton
+              aria-label="Previous"
+              icon={<FaStepBackward />}
+              variant="ghost"
+              color="white"
+              onClick={handlePrevious}
+              sx={{ "&:hover": { bg: "transparent" } }}
+              isDisabled={currentVideoIndex === 0} // Disable if at the first video
+            />
+            <IconButton
+              aria-label={isPlaying ? "Pause" : "Play"}
+              icon={isPlaying ? <FaPause /> : <FaPlay />}
+              variant="ghost"
+              color="white"
+              onClick={handlePlayPause}
+              sx={{ "&:hover": { bg: "transparent" } }}
+              mx={2}
+            />
+            <IconButton
+              aria-label="Next"
+              icon={<FaStepForward />}
+              variant="ghost"
+              color="white"
+              onClick={handleNext}
+              sx={{ "&:hover": { bg: "transparent" } }}
+              isDisabled={currentVideoIndex === videoList.length - 1} // Disable if at the last video
+            />
+          </Flex>
+        </Box>
       </Box>
     </Box>
-  </Box>
-);
-
-YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired,
+  );
 };
 
-function CustomComponent() {
-  return (
-    <div>
-      <YoutubeEmbed embedId="fBUfJFcxjiM" />
-    </div>
-  );
-}
+YoutubeEmbed.propTypes = {
+  initialEmbedId: PropTypes.string.isRequired,
+  videoList: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+const CustomComponent = () => (
+  <div>
+    <YoutubeEmbed
+      initialEmbedId="fBUfJFcxjiM"
+      videoList={["fBUfJFcxjiM", "3fumBcKC6RE", "eX2qFMC8cFo", "yzC4hFK5P3g"]}
+    />
+  </div>
+);
 
 export default CustomComponent;
