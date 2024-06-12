4b799a22a708c8001bacf813c8c891cb27fe1e2b
MONEYBAG <110569376+King-Lord@users.noreply.github.com>
2024-06-12 04:25:08 +0100

    customcomponent

--- src/components/CustomComponent.tsx
+++ src/components/CustomComponent.tsx
@@ -1,28 +1,98 @@
 import PropTypes from "prop-types";
+import {
+  Box,
+  Img,
+  Heading,
+  Flex,
+  IconButton,
+  AspectRatio,
+} from "@chakra-ui/react";
+import {
+  //  FaPlay,
+  //  FaPause,
+  FaStepForward,
+  FaStepBackward,
+} from "react-icons/fa";
+import Pattern from "../assets/images/Pattern.png";
 
 const YoutubeEmbed = ({ embedId }) => (
-  <div
-    className="
-  overflow-hidden;
-  pb-56.25%;
-  relative;
-  height-0"
-  >
+  <Box maxW="1024px" mx="auto" px={{ base: 4, md: 8 }} position="relative">
+    <Box
+      zIndex={1}
+      position="absolute"
+      right={{ base: -4, md: -12 }}
+      top={{ base: -4, md: -12 }}
+      width={{ base: "100px", md: "150px" }}
+    >
+      <Img src={Pattern} />
+    </Box>
+    <Heading as="h2" size="lg" fontWeight="bold" textAlign="center" mb={4}>
+      How to apply to join as instructor
+    </Heading>
+    {/* <YouTube videoId={videoId} /> */}
     <iframe
-      className="left-0
-  top-0
-  h-100%
-  w-100%
-  absolute"
-      // width="853"
-      // height="480"
+      width="1024"
+      height="515"
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
+      style={{ position: "relative" }}
       title="Embedded youtube"
     />
-  </div>
+    <Box
+      p={5}
+      bg="white"
+      borderRadius="22px"
+      boxShadow="lg"
+      position="relative"
+      zIndex={2}
+    >
+      <Box borderRadius="22px" overflow="hidden">
+        <AspectRatio ratio={16 / 9}>
+          <div id="youtube-player" />
+        </AspectRatio>
+        <Flex
+          position="absolute"
+          bottom={0}
+          left={0}
+          right={0}
+          align="center"
+          borderBottomRightRadius="22px"
+          borderBottomLeftRadius="22px"
+          justifyContent="center"
+          bg="rgba(0, 0, 0, 0.5)"
+          p={2}
+        >
+          <IconButton
+            aria-label="Previous"
+            icon={<FaStepBackward />}
+            variant="ghost"
+            color="white"
+            // onClick={handlePrevious}
+            sx={{ "&:hover": { bg: "transparent" } }}
+          />
+          <IconButton
+            aria-label={`{isPlaying ? "Pause" : "Play"}`}
+            // icon={`{isPlaying ? <FaPause /> : <FaPlay />}`}
+            variant="ghost"
+            color="white"
+            // onClick={handlePlayPause}
+            sx={{ "&:hover": { bg: "transparent" } }}
+            mx={2}
+          />
+          <IconButton
+            aria-label="Next"
+            icon={<FaStepForward />}
+            variant="ghost"
+            color="white"
+            // onClick={handleNext}
+            sx={{ "&:hover": { bg: "transparent" } }}
+          />
+        </Flex>
+      </Box>
+    </Box>
+  </Box>
 );
 
 YoutubeEmbed.propTypes = {
