0e303ddff789211fb5df445c364109d45b4372b6
Milton Black <arziblack2@gmail.com>
2024-06-23 18:32:04 +0100

    feat: student dashboard home halfway done...

--- src/pages/Student/views/Home.tsx
+++ src/pages/Student/views/Home.tsx
@@ -1,6 +1,21 @@
 import { useEffect, useState } from "react";
 import VideoEmbed from "../components/VideoComp";
 
+const homeCourses = [
+  {
+    course: "Pornography",
+    image: "image",
+    tutor: "Samuel",
+    status: "completed",
+  },
+  {
+    course: "Videography",
+    image: "image",
+    tutor: "Samuel",
+    status: "completed",
+  },
+];
+
 export const Home = () => {
   const [containerWidth, setContainerWidth] = useState<number>(660);
   const iframeHeight = containerWidth * (215 / 560);
@@ -27,20 +42,41 @@ export const Home = () => {
             improve your skills, and reach your goals at your own speed.
           </p>
         </div>
-        <div id="video-container" style={{ width: '100%', maxWidth: '100%' }} className="pt-7">
+        <div
+          id="video-container"
+          style={{ width: "100%", maxWidth: "100%" }}
+          className="pt-7"
+        >
           <VideoEmbed
             videoId={videoId}
             containerWidth={containerWidth}
             iframeHeight={iframeHeight}
           />
         </div>
-        <div>
-          <div>
-            <h2>My Courses</h2>
-            <h2>Status</h2>
-            <h2>All Courses</h2>
+        <div className="flex flex-col w-full mt-7 border border-gray-500 pt-4 pb-1 px-2 rounded">
+          <div className="flex w-full py-1">
+            <h2 className="w-2/4">My Courses</h2>
+            <h2 className="w-1/4">Status</h2>
+            <h2 className="w-1/4 text-right">All Courses</h2>
           </div>
-          <div></div>
+          {homeCourses.map((item, id) => (
+            <div
+              className="flex w-full items-center my-2 border border-gray-400 p-1 rounded"
+              key={id}
+            >
+              <div className="w-2/4 flex items-center">
+                <div className="bg-red-400 rounded-full h-9 w-9 p-1"></div>
+                <div className="flex flex-col h-full justify-between  ml-2">
+                  <h2 className="text-xs">{item.course}</h2>
+                  <h2 className="font-[100] text-xs">{item.tutor}</h2>
+                </div>
+              </div>
+              <h2 className="w-1/4 font-[100] text-xs">{item.status}</h2>
+              <button className="w-1/4 justify-end py-2 px-1 border border-gray-500 rounded">
+                View Courses
+              </button>
+            </div>
+          ))}
         </div>
       </div>
       <div className="w-1/3 flex justify-center items-center"></div>
