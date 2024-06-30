2fc91cb938c73a99bb9c64d8f7ab550f8d8606f4
Milton Black <arziblack2@gmail.com>
2024-06-30 22:46:45 +0100

    feat: integrated my tutor endpoint...

--- src/pages/Student/components/Tutors/Approved.tsx
+++ src/pages/Student/components/Tutors/Approved.tsx
@@ -4,12 +4,11 @@ import { useAppSelector } from "../../../../hooks/reactReduxHooks";
 
 export const Approved = () => {
   const { approvedTutors } = useAppSelector(state => state.student);
-  console.log(approvedTutors);
   const { data } = approvedTutors;
   return (
     <Box className="text-white ">
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={5} color="#ffffff">
-        {data.map((tutor, index) => (
+        {data?.map((tutor, index) => (
           <TutorCard
             key={index}
             name={tutor?.name}
--- src/pages/Student/components/Tutors/MyTutors.tsx
+++ src/pages/Student/components/Tutors/MyTutors.tsx
@@ -1,51 +1,20 @@
 import { Box, SimpleGrid } from "@chakra-ui/react";
 import { TutorCard } from "./TutorCard";
-import { TEMPLATE } from "../../../../constants/image";
-
-// Sample data for tutors
-const tutors = [
-  {
-    name: "Milton Mosiah",
-    bio: "Experienced frontend developer with a knack for teaching.",
-    course: "Frontend Development",
-    gender: "Male",
-    imageUrl: TEMPLATE,
-  },
-  {
-    name: "James Renault",
-    bio: "Seasoned developer with a strong background in full-stack development.",
-    course: "Full-Stack Development",
-    gender: "Male",
-    imageUrl: TEMPLATE,
-  },
-  {
-    name: "Bryan Lake",
-    bio: "Passionate about UI/UX design and frontend technologies.",
-    course: "UI/UX Design",
-    gender: "Male",
-    imageUrl: TEMPLATE,
-  },
-  {
-    name: "Reston Mix",
-    bio: "Dedicated to helping students excel in their coding journey.",
-    course: "Backend Development",
-    gender: "Female",
-    imageUrl: TEMPLATE,
-  },
-];
+import { useAppSelector } from "../../../../hooks/reactReduxHooks";
 
 export const MyTutors = () => {
+  const { myTutors } = useAppSelector(state => state.student);
   return (
     <Box className="text-white ">
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={5} color="#ffffff">
-        {tutors.map((tutor, index) => (
+        {myTutors?.data?.map((tutor, index) => (
           <TutorCard
             key={index}
-            name={tutor.name}
-            bio={tutor.bio}
-            course={tutor.course}
-            gender={tutor.gender}
-            imageUrl={tutor.imageUrl}
+            name={tutor?.name}
+            bio={tutor?.introduction}
+            course={tutor?.specialization}
+            gender={tutor?.sex}
+            imageUrl={tutor?.profileImage}
           />
         ))}
       </SimpleGrid>
