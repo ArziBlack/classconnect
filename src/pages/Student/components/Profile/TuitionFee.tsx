41efe032d994bc90630403dbba4edc10eebfb644
Milton Black <arziblack2@gmail.com>
2024-07-04 11:24:10 +0100

    feat: addes the tution fees to the settings view...

--- src/pages/Student/components/Profile/TuitionFee.tsx
+++ src/pages/Student/components/Profile/TuitionFee.tsx
@@ -5,7 +5,7 @@ export const TuitionFee = () => {
   const { data } = useAppSelector(tam => tam.auth)
   return (
     <Box className="text-white pt-6 ">
-      <h1 className="text-lg uppercase p-2 border border-grey max-w-[350px] rounded-md">Tuition Fee : {data?.paymentPlan}</h1>
+      <h1 className="text-lg uppercase p-2 border border-grey max-w-[320px] rounded-md">Tuition Fee : {data?.paymentPlan}</h1>
     </Box>
   );
 };
