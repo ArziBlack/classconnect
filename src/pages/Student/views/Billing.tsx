b6a9fd3590e581042c7af16838eec7499886dece
Milton Black <arziblack2@gmail.com>
2024-07-05 08:51:40 +0100

    feat: Billing started...

--- src/pages/Student/views/Billing.tsx
+++ src/pages/Student/views/Billing.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export const Billing = () => {
-  return <div>Billing</div>;
+  return (
+  <div className="flex w-full items-center pr-5 gap-5">
+    <div className="w-2/3 bg-[#023248] rounded-md flex flex-col h-full p-3">
+      <h2 className="py-2">Billing Details</h2>
+      <div></div>
+      <div className="flex flex-col items-start py-1">
+        <label className="py-1">First Name</label>
+        <input type="text" placeholder="First Name....." className="py-1 flex w-full bg-[#255E78] border-none"/>
+      </div>
+    </div>
+    <div className="w-1/3 bg-[#255E78]"></div>
+  </div>
+  );
 };
