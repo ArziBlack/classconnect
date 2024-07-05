39efa5fe070c7d191726a432915e2da76111574b
Milton Black <arziblack2@gmail.com>
2024-07-05 11:50:56 +0100

    feat: billing inprogress...

--- src/pages/Student/views/Billing.tsx
+++ src/pages/Student/views/Billing.tsx
@@ -1,17 +1,50 @@
 import React from "react";
+import CButton from "../../../components/Button";
 
 export const Billing = () => {
   return (
-  <div className="flex w-full items-center pr-5 gap-5">
-    <div className="w-2/3 bg-[#023248] rounded-md flex flex-col h-full p-3">
-      <h2 className="py-2">Billing Details</h2>
-      <div></div>
-      <div className="flex flex-col items-start py-1">
-        <label className="py-1">First Name</label>
-        <input type="text" placeholder="First Name....." className="py-1 flex w-full bg-[#255E78] border-none"/>
+    <div className="flex w-full items-center pr-5 gap-5">
+      <div className="w-2/3 bg-[#023248] rounded-md flex flex-col h-full p-5 min-h-[650px] justify-between">
+        <h2 className="py-2 text-white text-lg font-[600]">Billing Details</h2>
+        <div className="flex flex-col text-white py-2 text-sm">
+          <div className="flex flex-col items-start py-1">
+            <label className="py-1 font-light">First Name</label>
+            <input type="text" placeholder="First Name....." className="px-2 my-1 py-3 flex w-full bg-[#255E78] border-none rounded-md" />
+          </div>
+          <div className="flex flex-col items-start py-1">
+            <label className="py-1 font-light">Last Name</label>
+            <input type="text" placeholder="Last Name....." className="px-2 my-1 py-3 flex w-full bg-[#255E78] border-none rounded-md" />
+          </div>
+          <div className="flex flex-col items-start py-1">
+            <label className="py-1 font-light">Phone Number</label>
+            <input type="number" placeholder="Phone....." className="px-2 my-1 py-3 flex w-full bg-[#255E78] border-none rounded-md" />
+          </div>
+          <div className="flex flex-col items-start py-1">
+            <label className="py-1 font-light">Email</label>
+            <input type="number" placeholder="Your Email....." className="px-2 my-1 py-3 flex w-full bg-[#255E78] border-none rounded-md" />
+          </div>
+        </div>
+        <div className="bg-[#fff] w-full flex flex-col py-2 px-1 rounded-md font-light text-xs">
+          <h3 className="py-1 text-sm">Secured By Paystack</h3>
+          <div className="py-1 font-[600]">
+            <span>Paystack</span>
+          </div>
+        </div>
+        <CButton text="Pay Now" />
+      </div>
+      <div className="w-1/3 bg-[#255E78]">
+        <div className="flex flex-col p-3">
+          <h2>Your Plan</h2>
+          <div className="flex items-center justify-between w-full">
+            <div>
+            <img alt="plan"/>
+            </div>
+            <div></div>
+          </div>
+          <div></div>
+        </div>
+        <div className="flex flex-col p-3"></div>
       </div>
     </div>
-    <div className="w-1/3 bg-[#255E78]"></div>
-  </div>
   );
 };
