af31cda6820104b631f3b11cafee6823083f8ccc
Milton Black <arziblack2@gmail.com>
2024-07-13 16:39:08 +0100

    feat: change the loader backgound color...

--- src/utils/Loader.tsx
+++ src/utils/Loader.tsx
@@ -3,7 +3,7 @@ import "../styles/loader.css";
 
 const Loader = () => {
     return (
-        <div className='w-full flex items-center justify-center h-screen bg-black'>
+        <div className='w-full flex items-center justify-center h-screen bg-[#002333]'>
             <div className="honeycomb">
                 <div></div>
                 <div></div>
