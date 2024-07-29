6191901a1a7981f41f29c8778933245228a5d4df
Milton Black <arziblack2@gmail.com>
2024-07-29 17:22:05 +0100

    feat: added refresh to the student view in tutor route...

--- src/pages/Instructor/views/Students.tsx
+++ src/pages/Instructor/views/Students.tsx
@@ -3,15 +3,27 @@ import { BreadCrumb } from "../components/Courses/BreadCrumb";
 import { useEffect } from "react";
 import { useAppDispatch } from "../../../hooks/reactReduxHooks";
 import { getMyStudents } from "../../../services/tutor/tutorThunk";
+import Refresh from "../../../components/Refresh";
+import useCustomToast from "../../../hooks/useCustomToast";
 
 const links = [{ to: "", label: "My Students" }];
 
 export const Students = () => {
+  const toast = useCustomToast();
   const dispatch = useAppDispatch();
   useEffect(() => {
     document.title = "HEP Tutors - My Students";
     dispatch(getMyStudents());
   }, [dispatch]);
+
+  const handleRefresh = async () => {
+    const response = await dispatch(getMyStudents());
+    if (getMyStudents.fulfilled.match(response)) {
+      toast("Refreshed", "info");
+    } else if (getMyStudents.rejected.match(response)) {
+      toast(response?.payload, "warning");
+    }
+  }
 
   return (
     <>
@@ -19,6 +31,7 @@ export const Students = () => {
         title="Students"
         subtext="Access your student list and interact with your students. View their contact information, office hours, and schedule one-on-one sessions to support their learning experience."
       />
+      <Refresh handleRefresh={handleRefresh}/>
       <BreadCrumb links={links} />
     </>
   );
